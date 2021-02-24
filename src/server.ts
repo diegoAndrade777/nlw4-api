dea44f6e0b90c46654c549360ca8540ba8b41062
diegoAndrade777 <diego.rodrigo.andrade@gmail.com>
2021-02-24 15:00:20 -0300

    feat: repositories and tests

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,6 @@
-import 'reflect-metadata';
-import express from 'express';
-import './database';
-import { router } from './routes';
+import { app } from "./app";
 
 const port = process.env.PORT || 3333;
-
-const app = express();
-
-app.use(express.json())
-app.use(router)
 
 app.listen(port, () => {
   console.log('Server running at port: ', port);
